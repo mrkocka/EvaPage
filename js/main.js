e6ddedc9196267181996b15f90a173f352cab01f
Mr. Joe <radics.pepe@gmail.com>
2022-05-08 15:51:17 +0200

    save2_05.08

--- js/main.js
+++ js/main.js
@@ -1,4 +1,26 @@
-// ScrollTopp Button
+
+
+
+// Menu Button
+
+     const openButton =  document.getElementById('js-openButton');
+     const mobilMenu = document.getElementById('js-mobilMenu');
+
+     openButton.addEventListener('click' , OpenMenu);
+
+     function OpenMenu(){
+         mobilMenu.style.transform = 'translateY(0)';
+         /* mobilMenu.classList.toggle('menu-active')
+          openButton.classList.toggle('fa-caret-square-o-down')
+         openButton.classList.add('fa-minus-square-o') */
+         
+        
+         
+     }
+
+
+
+/* // ScrollTopp Button
     $(window).scroll(function(){
         if($(this).scrollTop() > 150)
             $(".gotopbtn").addClass("active");
@@ -7,16 +29,4 @@
     });
     $('.gotopbtn').click(function(){
         $("html, body").animate({scrollTop:0})
-    });
-
-
-// Menu Button
-     let OpenButton =  document.getElementById("js-OpenButton");
-
-     OpenButton.addEventListener("click" , OpenMenu);
-
-     function OpenMenu(){
-         OpenButton.classList.toggle('fa-caret-square-o-down')
-         OpenButton.classList.add('fa-minus-square-o')
-         
-     }
+    }); */
